751b50d160d2ff67a4b8b97fd54d4533f8770abd
ruhuma <141302425+ruhuma@users.noreply.github.com>
2024-02-14 17:34:08 +0000

    14 feb

--- app.py
+++ app.py
@@ -13,13 +13,16 @@ def Hello():
 
 @app.route('/classification', methods = ['GET','POST'])
 def hey():
-    try:
         if request.method == 'POST':
-            print('1')
+            print('1 starting')
             selected_month = float(request.form['month'])
+            print('month',selected_month)
             age = float(request.form['age'])
-            occupation = float(request.form['occupation'])
+            print('age',age)
+            occupation = request.form['occupation']
+            
             annualincome = float(request.form['annualincome'])
+            print('annualincome',annualincome)
             num_bank_accounts = float(request.form['num_bank_accounts'])
             num_credit_card = float(request.form['num_credit_card'])
             interest_rate = float(request.form['interest_rate'])
@@ -39,9 +42,19 @@ def hey():
             monthly_balance = float(request.form['monthly_balance'])
             payment_behaviour = float(request.form['payment_behaviour'])
             print(payment_behaviour)
-            input_array = np.array([[selected_month,age,occupation,annualincome,num_bank_accounts,num_credit_card,interest_rate
+            print(occupation)
+            with open('label_encoder_1.pkl', 'rb') as file:
+                label_encoder = pickle.load(file)
+            occupation_encoded = label_encoder.transform(np.array([[occupation]]))
+            print('encoded',occupation_encoded)
+            occupation_encoded_d = occupation_encoded[0].astype(np.float64)
+            print(occupation_encoded_d)
+            print(monthly_balance)
+
+            input_array = np.array([[selected_month,age,occupation_encoded_d,annualincome,num_bank_accounts,num_credit_card,interest_rate
                                      ,num_loans,delay_due_date,delay_payments,changed_credit_limit,credit_inquiry,credit_mix,outstanding_debts,credit_ration,credit_history_age,minimum_amount,
                                      EMI_per_month,amount_invested_monthly,payment_behaviour,monthly_balance]]).astype(np.float64)
+            
             model = pickle.load(open('best_rf_model.pkl','rb'))
             scaler = joblib.load("stdscaler_CS.pkl")
             input_scaled_G = scaler.transform(input_array)
@@ -63,9 +76,7 @@ def hey():
 
         else:
             return render_template('classification.html')
-    except ValueError:
-        # Handle the case where the input is not a valid float
-        return "Invalid input. Please enter a valid value in the  fields."
+    
 
 if __name__ == '__main__':
     app.run()
