eaacdf15b28463d9b621d465fedd5b540347a695
ruhuma <141302425+ruhuma@users.noreply.github.com>
2024-02-14 04:22:52 +0000

    first

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib
+import pandas as pd
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def Hello():
+    return render_template('index.html')
+
+@app.route('/classification', methods = ['GET','POST'])
+def hey():
+    try:
+        if request.method == 'POST':
+            
+    except ValueError:
+        # Handle the case where the input is not a valid float
+        return "Invalid input. Please enter a valid value in the  fields."
+
+if __name__ == '__main__':
+    app.run()
